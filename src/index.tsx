767360f891ead0f399180d1be6fe31230d0eb546
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2023-07-26 20:31:24 +0900

    anchor

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,26 @@
 import { render } from 'preact';
 import { LocationProvider, Router, Route, lazy } from 'preact-iso';
+
 import { Home } from './pages/Home';
 import { NotFound } from './pages/_404';
 // import './style.css';
 
 export function App() {
   return (
-    <LocationProvider>
-      <Router>
-        <Route path="/" component={Home} />
-        <Route path="/ag-grid" component={lazy(() => import('./pages/AgGrid'))} />
-        <Route default component={NotFound} />
-      </Router>
-    </LocationProvider>
+    <>
+      <div style="display: flex; flex-direction: column;">
+        <a href='/'>HOME</a>
+        <a href='/ag-grid'>AG-GRID</a>
+        <a href='#' onClick={() => location.href = '/ag-grid'}>AG-GRID by location.href</a>
+      </div>
+      <LocationProvider>
+        <Router>
+          <Route path="/" component={Home} />
+          <Route path="/ag-grid" component={lazy(() => import('./pages/AgGrid'))} />
+          <Route default component={NotFound} />
+        </Router>
+      </LocationProvider>
+    </>
   );
 }
 
